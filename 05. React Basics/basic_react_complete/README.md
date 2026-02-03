2058214c74447b2094d9ab99ab846e929a0dce45
JonaCodes <jonathanfarache@gmail.com>
2026-02-03 15:40:55 +0200

    cleanup

--- 05. React Basics/basic_react_complete/README.md
+++
@@ -1,1 +0,0 @@
-Intro presentation to react: https://gamma.app/docs/Introduction-to-React-7j7hu91u82j09ys
--- 05. React Basics/memory_game_complete/src/MessageView.tsx
+++
@@ -1,5 +0,0 @@
-type Props = { message: string };
-
-export default function MessageView({ message }: Props) {
-  return <p className="message">{message}</p>;
-}

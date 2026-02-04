79da628755ef4ec737f54941001c72eaf47e2d79
JonaCodes <jonathanfarache@gmail.com>
2026-02-04 07:54:04 +0200

    ex 1 instructions

--- 06. React Practice/src/ex-1/Ex1.tsx
+++ 06. React Practice/src/ex-1/Ex1.tsx
@@ -1,13 +1,20 @@
-import KpiHeader from "./KpiHeader";
-import KpiTagline from "./KpiTagline";
-import KpiValue from "./KpiValue";
-
+/**
+ * Simple components exercise.
+ * Create a KpiHeader, KpiValue and KpiTagline components, one per file.
+ 
+ * In the value component, make sure the value itself is stored in a `const`:
+ * const value = 1000;
+ * and then render this value in the component.
+ 
+ * In the tagline component, store the percent change as a const as well
+ * Also, color the tagline green or red depending on the sign of the change.
+ * 
+ * Note the css classes in `ex1.css` to help you build it "prettily"
+ */
 export default function Ex1() {
   return (
     <div className="kpi-card-container">
-      <KpiHeader />
-      <KpiValue />
-      <KpiTagline />
+      Create the components and render them here!
     </div>
   );
 }
--- 06. React Practice/src/ex-1/KpiValue.tsx
+++
@@ -1,9 +0,0 @@
-export default function KpiValue() {
-  const value = 1000;
-
-  return (
-    <>
-      <h3>${value.toLocaleString()}</h3>
-    </>
-  );
-}
